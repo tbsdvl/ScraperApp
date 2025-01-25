7eef6318e79a6de45ee98a00df51c7532c8cb61a
Triston Burns <tburns@aurasolutionsllc.com>
2025-01-24 23:03:34 -0700

    Add stylecop & create db context

--- ScraperApp.Infrastructure/Data/ScraperDbContext.cs
+++ ScraperApp.Infrastructure/Data/ScraperDbContext.cs
@@ -1,6 +1,30 @@
-﻿namespace ScraperApp.Infrastructure.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ScraperApp.Infrastructure.Data
 {
-    public class ScraperDbContext
+    /// <summary>
+    /// Represents the scraper db context.
+    /// </summary>
+    public class ScraperDbContext : DbContext
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScraperDbContext"/> class.
+        /// </summary>
+        /// <param name="dbOptions">The database context options.</param>
+        public ScraperDbContext(DbContextOptions<ScraperDbContext> dbOptions)
+            : base(dbOptions)
+        {
+        }
+
+        /// <summary>
+        /// Configures the schema for the context.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // configure entities here
+        }
     }
 }
