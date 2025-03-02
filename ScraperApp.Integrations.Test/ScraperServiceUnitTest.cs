c835361d9d6b95e0ff8df1cd3607fecf9203e824
Triston Burns <tburns@aurasolutionsllc.com>
2025-03-02 10:52:36 -0700

    Rename service

---
+++ ScraperApp.Integrations.Test/ScraperServiceUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using ScraperApp.ApplicationCore.Interfaces;
+
+namespace ScraperApp.Integrations.Test
+{
+    [TestClass]
+    public sealed class ScraperServiceUnitTest
+    {
+        private IScraperService ScraperService { get; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetPage_Succeeds()
+        {
+
+        }
+    }
+}
