7e9a26ee56b12a4adca12cdc2f11ba4f50e0291e
Triston Burns <tburns@aurasolutionsllc.com>
2025-03-02 11:40:12 -0700

    Add Automapper dependency injection to program, add fail case to integration test class

--- ScraperApp.Integrations.Test/ScraperServiceUnitTest.cs
+++ ScraperApp.Integrations.Test/ScraperServiceUnitTest.cs
@@ -1,22 +1,69 @@
-﻿using ScraperApp.ApplicationCore.Interfaces;
+﻿using AutoMapper;
+using ScraperApp.ApplicationCore;
+using ScraperApp.ApplicationCore.Enums;
+using ScraperApp.ApplicationCore.Models;
+using ScraperApp.ApplicationCore.Services;
 
 namespace ScraperApp.Integrations.Test
 {
     [TestClass]
     public sealed class ScraperServiceUnitTest
     {
-        private IScraperService ScraperService { get; }
+        private IMapper Mapper { get; set; }
+        private ScraperService ScraperService { get; set; }
 
         [TestInitialize]
         public void Initialize()
         {
+            var mappingConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<AutoMapperProfile>(); // Register the AutoMapper profile
+            });
 
+            this.Mapper = mappingConfig.CreateMapper();
+
+            this.ScraperService = new ScraperService(this.Mapper);
+        }
+
+        private static ScraperRequest GetScraperRequest()
+        {
+            return new ScraperRequest()
+            {
+                Url = "https://ebay.com",
+                Options = new QueryOptions()
+                {
+                    QueryOptionsType = (int)QueryOptionsTypeEnum.Ebay,
+                },
+            };
         }
 
         [TestMethod]
-        public void GetPage_Succeeds()
+        public async Task GetPage_Succeeds()
         {
+            // Arrange
+            var request = GetScraperRequest();
 
+            // Act
+            var result = await this.ScraperService.GetPageHtml(request);
+
+            // Assert
+            Assert.IsTrue(result.Succeeded);
+            Assert.IsTrue(result.Data != null);
+        }
+
+        [TestMethod]
+        public async Task GetPage_No_Query_Options_Type_Fails()
+        {
+            // Arrange
+            var request = GetScraperRequest();
+            request.Options.QueryOptionsType = 0;
+
+            // Act
+            var result = await this.ScraperService.GetPageHtml(request);
+
+            // Assert
+            Assert.IsTrue(result.Succeeded);
+            Assert.IsTrue(result.Data != null);
         }
     }
 }
