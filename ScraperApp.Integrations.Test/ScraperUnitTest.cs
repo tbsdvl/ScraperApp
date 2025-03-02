69fd0a5a18cf38d1f7adf9691da7a9a3b2fd6109
Triston Burns <tburns@aurasolutionsllc.com>
2025-03-02 10:23:01 -0700

    Add integration tests project

---
+++ ScraperApp.Integrations.Test/ScraperUnitTest.cs
@@ -0,0 +1,12 @@
+﻿namespace ScraperApp.Integrations.Test
+{
+    [TestClass]
+    public sealed class ScraperUnitTest
+    {
+        [TestMethod]
+        public void GetPage_Succeeds()
+        {
+
+        }
+    }
+}
