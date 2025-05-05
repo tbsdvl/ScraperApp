2c35cf512d17006fa600fb3fee1deed00a2e5565
Triston Burns <tburns@aurasolutionsllc.com>
2025-05-04 17:44:05 -0700

    init ApplicationDbContext

---
+++ ScraperApp.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="ApplicationDbContext.cs" company="Psybersimian LLC">
+// Copyright (c) Psybersimian LLC. All rights reserved.
+// </copyright>
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ScraperApp.Infrastructure
+{
+    /// <summary>
+    /// Represents the application database context.
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // dbsets go here
+    }
+}
