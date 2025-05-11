61e74016a05d7e97ec150f24d08975dd9f1a7e7b
Triston Burns <tburns@aurasolutionsllc.com>
2025-05-11 11:55:12 -0700

    Add role entity

---
+++ Listopotamus.ApplicationCore/Entities/Identity/Role.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="Role.cs" company="Psybersimian LLC">
+// Copyright (c) Psybersimian LLC. All rights reserved.
+// </copyright>
+
+using Microsoft.AspNetCore.Identity;
+
+namespace Listopotamus.ApplicationCore.Entities.Identity
+{
+    /// <summary>
+    /// Represents a role in the identity system.
+    /// </summary>
+    public class Role : IdentityRole<int>
+    {
+    }
+}
