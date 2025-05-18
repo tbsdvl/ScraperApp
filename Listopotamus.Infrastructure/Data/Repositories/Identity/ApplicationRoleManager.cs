78d30b8ecc5c2f620f500c16a49143777f2bcc12
Triston Burns <tburns@aurasolutionsllc.com>
2025-05-18 14:06:21 -0700

    Create application role manager

---
+++ Listopotamus.Infrastructure/Data/Repositories/Identity/ApplicationRoleManager.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="ApplicationRoleManager.cs" company="Psybersimian LLC">
+// Copyright (c) Psybersimian LLC. All rights reserved.
+// </copyright>
+
+using Listopotamus.Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace Listopotamus.Infrastructure.Data.Repositories.Identity
+{
+    /// <summary>
+    /// Represents the application role manager.
+    /// </summary>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="ApplicationRoleManager"/> class.
+    /// </remarks>
+    /// <param name="store">The role store.</param>
+    /// <param name="roleValidators">The list of role validators.</param>
+    /// <param name="keyNormalizer">The key normalizer.</param>
+    /// <param name="errors">The list of errors.</param>
+    /// <param name="logger">The logger.</param>
+    public class ApplicationRoleManager(
+        IRoleStore<Role> store,
+        IEnumerable<IRoleValidator<Role>> roleValidators,
+        ILookupNormalizer keyNormalizer,
+        IdentityErrorDescriber errors,
+        ILogger<RoleManager<Role>> logger) : RoleManager<Role>(store, roleValidators, keyNormalizer, errors, logger)
+    {
+    }
+}
