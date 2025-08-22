abdcb8e49ef0964cd753ff3b78dc119600fede95
Triston Burns <tburns@aurasolutionsllc.com>
2025-08-21 17:50:50 -0700

    Create new scrape job entity

--- Listopotamus.Core/Entities/BaseEntity.cs
+++ Listopotamus.Core/Entities/BaseEntity.cs
@@ -14,5 +14,25 @@ namespace Listopotamus.Core.Entities
         /// Gets or sets the id.
         /// </summary>
         public T Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created date.
+        /// </summary>
+        public DateTime? CreatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created by.
+        /// </summary>
+        public string CreatedBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the updated date.
+        /// </summary>
+        public DateTime? UpdatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the updated by.
+        /// </summary>
+        public string UpdatedBy { get; set; }
     }
 }
---
+++ Listopotamus.Core/Entities/Jobs/ScrapeJob.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="ScrapeJob.cs" company="Psybersimian LLC">
+// Copyright (c) Psybersimian LLC. All rights reserved.
+// </copyright>
+
+namespace Listopotamus.Core.Entities.Jobs
+{
+    /// <summary>
+    /// Represents a scrape job.
+    /// </summary>
+    public class ScrapeJob : BaseExternalEntity<long?>
+    {
+        /// <summary>
+        /// Gets or sets the search query id.
+        /// </summary>
+        public long? SearchQueryId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status.
+        /// </summary>
+        public int? Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the progress.
+        /// </summary>
+        public int Progress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the error message.
+        /// </summary>
+        public string? ErrorMessage { get; set; }
+    }
+
+}
